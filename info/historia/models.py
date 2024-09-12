c2bb32aaba562e4f58a69734aa14cd8a35d798f1
mauricio5225 <mauroidolo5225@gmail.com>
2024-09-12 17:23:41 -0300

    cambios en tpf

--- info/historia/models.py
+++ info/historia/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Padron(models.Model): #aplico una herencia
+    nombre = models.CharField(max_length=30)
+    apellido = models.CharField(max_length=30)
+    fecha_alta = models.DateField("Fecha de Publicacíon")
